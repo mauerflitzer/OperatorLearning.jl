b9dd0cccec133a55aa3fe124dc7b2128b2e7e72f
friedrtob <mauerflitzaz@gmail.com>
2022-06-29 10:23:33 +0200

    add the first draft of pinn loss

--- examples/burgers_DeepONet.jl
+++ examples/burgers_DeepONet.jl
@@ -1,7 +1,36 @@
-using Flux: length, reshape, train!, throttle, @epochs
+using Flux: length, reshape, train!, throttle, @epochs, params
+using Zygote: jacobian, gradient, hessian
 using OperatorLearning, Flux, MAT
+using Zygote
+using GalacticOptim, GalacticOptimJL
+using ModelingToolkit
+using ModelingToolkit: Interval
+using Dates
+using LinearAlgebra
+using OMEinsum
 
-device = cpu;
+
+device = gpu;
+
+
+#TODO modelling:
+# @parameters t, x
+# @variables u(..)
+# Dt = Differential(t)
+# Dx = Differential(x)
+# Dxx = Differential(x)^2
+
+# eq = Dt(u(t, x)) + u(t, x) * Dx(u(t, x)) - Dxx(u(t, x)) ~ 0
+
+# # Initial and boundary conditions
+# bcs = [u(0, x) ~ -sin(pi * x),
+#     u(t, 0) ~ 0.0,
+#     u(t, 1) ~ 0.0,
+#     u(t, 0) ~ u(t, 0)]
+
+# # Space and time domains
+# domains = [t ∈ Interval(0.0, 1.0),
+#     x ∈ Interval(0.0, 1.0)]
 
 #=
 We would like to implement and train a DeepONet that infers the solution
@@ -48,12 +77,47 @@ parameters = params(model)
 # The loss function
 # We can't use the "vanilla" implementation of the mse here since we have
 # two distinct inputs to our DeepONet, so we wrap them into a tuple
-loss(xtrain,ytrain,sensor) = Flux.Losses.mse(model(xtrain,sensor),ytrain)
+
+
+function modell_call(x, x_grid, t_grid)
+    model(x, vcat(x_grid, t_grid))
+end
+
+
+
+function loss_neu(xtrain, x_sensor, t_sensor, ytrain)
+    f = modell_call(xtrain, x_sensor, t_sensor)
+    #print("Before: $(Dates.format(now(), "HH:MM:SS"))\n")
+    f_t = reshape(jacobian(t_grid->modell_call(xtrain,x_sensor,t_sensor), t_grid)[1], (batch_size, sensor_count, sensor_count))
+    f_x = reshape(jacobian(x_grid->modell_call(xtrain,x_sensor,t_sensor), x_grid)[1], (batch_size, sensor_count, sensor_count))
+    #print("Jacobians done: $(Dates.format(now(), "HH:MM:SS"))\n")
+    f_xx_diag = Float64[]
+    for j in 1:batch_size
+        for i in 1:sensor_count
+            push!(f_xx_diag,diaghessian(x_sensor->modell_call(xtrain, x_sensor, t_grid)[i], x_grid)[1][i])
+        end
+    end
+    f_xx_diag = reshape(f_xx_diag,(batch_size, sensor_count))
+    f_t = mapslices(diag, f_t, dims=[2,3])
+    f_x = mapslices(diag, f_x, dims=[2,3])
+    #print("After: $(Dates.format(now(), "HH:MM:SS"))\n")
+    @ein out[batch, x] := f[batch, x] * f_x[batch, x, 1]
+    out = out + f_t - f_xx_diag
+    return 20* Flux.Losses.mse(zeros(batch_size, sensor_count), out) + Flux.Losses.mse(model(xtrain, sensor), ytrain)
+end
+
+
+
+
+loss(xtrain, ytrain, sensor) = Flux.Losses.mse(model(xtrain, sensor), ytrain)
 
 # Define a callback function that gives some output during training
-evalcb() = @show(loss(xtest,ytest,grid))
+evalcb() = @show(loss(xtest, ytest, grid))
 # Print the callback only every 5 seconds
 throttled_cb = throttle(evalcb, 5)
+#NeuralPDE workflow
+# res = GalacticOptim.solve(prob, opt, progress=false; cb=evalcb, maxiters=100)
 
 # Do the training loop
-Flux.@epochs 500 train!(loss, parameters, [(xtrain,ytrain,grid)], opt, cb = evalcb)
+#Flux.@epochs 500 train!(loss, parameters, [(xtrain, ytrain, grid)], opt, cb=evalcb)
+Flux.@epochs 10 train!(loss_neu, parameters, [(xtrain, x_grid, t_grid, ytrain)], opt, cb=evalcb)
