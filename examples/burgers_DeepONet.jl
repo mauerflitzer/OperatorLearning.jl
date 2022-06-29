1cdcfd8a54160640a760b219832ab0bd2cdba8fb
friedrtob <mauerflitzaz@gmail.com>
2022-06-29 10:26:58 +0200

    Change the resolution to 64 sensors

--- examples/burgers_DeepONet.jl
+++ examples/burgers_DeepONet.jl
@@ -45,27 +45,30 @@ vars = matread("burgers_data_R10.mat") |> device
 
 # For trial purposes, we might want to train with different resolutions
 # So we sample only every n-th element
-subsample = 2^3;
-
+subsample = 2^7;
+batch_size = 10
 # create the x training array, according to our desired grid size
-xtrain = vars["a"][1:1000, 1:subsample:end]' |> device;
+xtrain = vars["a"][1:batch_size, 1:subsample:end]' |> device;
 # create the x test array
-xtest = vars["a"][end-99:end, 1:subsample:end]' |> device;
+xtest = vars["a"][end-batch_size:end, 1:subsample:end]' |> device;
 
 # Create the y training array
-ytrain = vars["u"][1:1000, 1:subsample:end] |> device;
+ytrain = vars["u"][1:batch_size, 1:subsample:end] |> device;
 # Create the y test array
-ytest = vars["u"][end-99:end, 1:subsample:end] |> device;
+ytest = vars["u"][end-batch_size:end, 1:subsample:end] |> device;
 
 # The data is missing grid data, so we create it
 # `collect` converts data type `range` into an array
-grid = collect(range(0, 1, length=1024))' |> device
-
+# vcat stacks the 2 grid vectors
+sensor_count = 64
+x_grid = collect(range(0, 1, length=sensor_count))'
+t_grid = ones(1,sensor_count)
+grid = vcat(x_grid, t_grid) |> device
 # Create the DeepONet:
 # IC is given on grid of 1024 points, and we solve for a fixed time t in one
 # spatial dimension x, making the branch input of size 1024 and trunk size 1
 # We choose GeLU activation for both subnets
-model = DeepONet((1024,1024,1024),(1,1024,1024),gelu,gelu) |> device
+model = DeepONet((sensor_count, sensor_count, sensor_count), (2, sensor_count, sensor_count), gelu, gelu) |> device
 
 # We use the ADAM optimizer for training
 learning_rate = 0.001
