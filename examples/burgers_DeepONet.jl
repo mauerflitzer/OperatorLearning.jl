ff0c6fd1ef6f4224ff4464dba89edc0cea88e1b0
friedrtob <mauerflitzaz@gmail.com>
2022-07-31 14:35:20 +0200

    New loss for testing

--- examples/burgers_DeepONet.jl
+++ examples/burgers_DeepONet.jl
@@ -45,22 +45,22 @@ vars = matread("burgers_data_R10.mat") |> device
 
 # For trial purposes, we might want to train with different resolutions
 # So we sample only every n-th element
-subsample = 2^7;
+subsample = 2^10;
 batch_size = 10
 # create the x training array, according to our desired grid size
-xtrain = vars["a"][1:batch_size, 1:subsample:end]' |> device;
+xtrain = vars["a"][1, 1:subsample:end] |> device;
 # create the x test array
-xtest = vars["a"][end-batch_size:end, 1:subsample:end]' |> device;
+xtest = vars["a"][end-batch_size:end, 1:subsample:end] |> device;
 
 # Create the y training array
-ytrain = vars["u"][1:batch_size, 1:subsample:end] |> device;
+ytrain = vars["u"][1, 1:subsample:end]' |> device;
 # Create the y test array
-ytest = vars["u"][end-batch_size:end, 1:subsample:end] |> device;
+ytest = vars["u"][end-batch_size:end, 1:subsample:end]' |> device;
 
 # The data is missing grid data, so we create it
 # `collect` converts data type `range` into an array
 # vcat stacks the 2 grid vectors
-sensor_count = 64
+sensor_count = 8
 x_grid = collect(range(0, 1, length=sensor_count))'  |> device;
 t_grid = ones(1,sensor_count)  |> device;
 grid = vcat(x_grid, t_grid) |> device
@@ -77,16 +77,40 @@ opt = ADAM(learning_rate)
 # Specify the model parameters
 parameters = params(model)
 
+
+#NeuralPDE workflow
+# function loss_function_(θ)
+#     return loss_function__(θ)
+# end
+# function discretize(pde_system::PDESystem, discretization::DeepONet)
+#     f = OptimizationFunction(discretization, GalacticOptim.AutoZygote())
+#     GalacticOptim.OptimizationProblem(f, discretized_functions.flat_initθ)
+# end
+
 # The loss function
 # We can't use the "vanilla" implementation of the mse here since we have
 # two distinct inputs to our DeepONet, so we wrap them into a tuple
 
+#NeuralPDE workflow
+#@named pdeSystem = PDESystem(eq, bcs, domains, [t, x], [u])
+#prob = discretize(pdeSystem, model(xtrain, grid))
 
+function loss_ics(model::DeepONet, xtrain, ytrain, sensor)
+    model(xtrain, grid)
+end
 function modell_call(x, x_grid, t_grid)
     model(x, vcat(x_grid, t_grid))
 end
+function modell_call_x(x, x_grid)
+    model(x, vcat(x_grid, t_grid))
+end
+function modell_call_t(x, t_grid)
+    model(x, vcat(x_grid, t_grid))
+end
 
-
+function modell_call2(x_grid)
+    return model(x_train, vcat(x_grid, t_grid))
+end
 
 function loss_neu(xtrain, x_sensor, t_sensor, ytrain)
     f = modell_call(xtrain, x_sensor, t_sensor)
@@ -94,7 +118,7 @@ function loss_neu(xtrain, x_sensor, t_sensor, ytrain)
     f_t = reshape(jacobian(t_grid->modell_call(xtrain,x_sensor,t_sensor), t_grid)[1], (batch_size, sensor_count, sensor_count))
     f_x = reshape(jacobian(x_grid->modell_call(xtrain,x_sensor,t_sensor), x_grid)[1], (batch_size, sensor_count, sensor_count))
     #print("Jacobians done: $(Dates.format(now(), "HH:MM:SS"))\n")
-    f_xx_diag = Float64[]
+    f_xx_diag = Float64[] 
     for j in 1:batch_size
         for i in 1:sensor_count
             push!(f_xx_diag,diaghessian(x_sensor->modell_call(xtrain, x_sensor, t_grid)[i], x_grid)[1][i])
@@ -114,7 +138,24 @@ function loss_neu(xtrain, x_sensor, t_sensor, ytrain)
     return 20* Flux.Losses.mse(comp, out) + Flux.Losses.mse(model(xtrain, sensor), ytrain)
 end
 
+function loss_refactored(xtrain, x_sensor, t_sensor, ytrain)
+    #input: nur ein sample, kein batch
+    #TODO: batch dimension handling hinzufügen
+    f = model(xtrain, vcat(x_sensor, t_sensor))
+    #Ableitungen erster Ordnung
+    f_x = jacobian(x_grid -> modell_call_x(xtrain, x_grid), x_sensor)[1]
+    f_t = jacobian(t_grid -> modell_call_x(xtrain, t_grid), t_sensor)[1]
+    #Ableitungen zweiter Ordnung
+    f_xx = zeros(sensor_count, sensor_count)
+    for i in 1:sensor_count
+        f_xx[i,:] =diaghessian(x_grid -> modell_call_x(xtrain, x_grid)[i], x_grid)[1]
+    end
 
+    #Lossberechnung
+    loss_residual = f_t + f .* f_x + f_xx
+    return 5 * sum(loss_residual) + Flux.Losses.mse(model(xtrain, vcat(x_sensor, t_sensor)), ytrain)
+end
+    
 
 
 loss(xtrain, ytrain, sensor) = Flux.Losses.mse(model(xtrain, sensor), ytrain)
